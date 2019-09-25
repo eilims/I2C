a6438ec626b549f7327a4381cfee95db17343273
Daniel Benusovich <dbenusovich@gmail.com>
2019-09-25 00:20:07 -0700

    Add logic for start and end bits

--- I2C/I2C.ino
+++ I2C/I2C.ino
@@ -1,5 +1,11 @@
  class I2C {
   private:
+    enum State {
+      WAIT,
+      START,
+      DATA
+    };
+
     uint8_t sda;
     uint8_t scl;
     uint8_t address;
@@ -9,12 +15,20 @@
     
     uint8_t currentBit;
     uint32_t index;
-    bool clk;
+    State state;
+    bool prevClk;
+    bool prevData;
 
 
-  void storeBit(int value){
+  bool storeBit(int value){
     buff[index] = (buff[index] << 1) | (value & 1);
     currentBit = (currentBit + 1) % 8;
+    bool byteComplete = false;
+    if(currentBit == 0)
+    {
+      byteComplete = true;
+    }
+    return byteComplete;
   }
 
   void printBuffer(){
@@ -41,7 +55,10 @@
     address(address),
     currentBit(0),
     index(0),
-    buffSize(bufferSize)
+    buffSize(bufferSize),
+    prevClk(false),
+    prevData(false),
+    state(State::WAIT)
     {
       this->buff = new uint8_t[bufferSize];
     }
@@ -71,27 +88,57 @@
     }
 
     void poll(){
-      if(digitalRead(scl) == 1 && clk == 0){
-        clk = 1;
-        int val = digitalRead(sda);
-        Serial.print("SDA: ");
-        Serial.println(val);
-        // Write to buffer
-        storeBit(val);
-        if(currentBit == 0)
-        {
-          Serial.println("Byte Received");
-          Serial.print("Data: ");
-          Serial.println(buff[index], BIN);
-          index++;
-          if(index == buffSize){
-            printBuffer();
+      bool clk = digitalRead(scl);
+      bool data = digitalRead(sda);
+      switch(state)
+      {
+        case WAIT:
+          // Start condition triggers when the sda line is changed low while the scl
+          // is one
+          if(clk == 1 && prevClk == 1 && !data && prevData)
+          {
+            Serial.println("START BIT RECEIVED");
+            currentBit = 0;
             index = 0;
+            state = START;
           }
-        }
-      } else if(digitalRead(scl) == 0 && clk == 1){
-        clk = 0;
+          break;
+        case START:
+          state = DATA;
+          break;
+        case DATA:
+          // Only update data on the rising edge
+          if(clk == 1 && prevClk == 0)
+          {
+            Serial.print("SDA: ");
+            Serial.println(data);
+            // Write to buffer
+            // Will return true if a byte was completed
+            if(storeBit(data))
+            {
+              Serial.println("Byte Received");
+              Serial.print("Data: ");
+              Serial.println(buff[index], BIN);
+              index++;
+              if(index == buffSize)
+              {
+                printBuffer();
+                index = 0;
+              }
+            }
+          }
+          // Start condition triggers when the sda line is changed high while the scl
+          // is one
+          else if(clk == 1 && prevClk == 1 && data && !prevData)
+          {
+            Serial.println("STOP BIT RECEIEVED");
+            state = WAIT;
+          }
+          break;
       }
+      // Store current data
+      prevClk = clk;
+      prevData = data;
     }
  };
 
--- I2C_Test/I2C_Test.ino
+++ I2C_Test/I2C_Test.ino
@@ -1,26 +1,46 @@
+int period = 20;
+
 void setup() {
   // put your setup code here, to run once:
   Serial.begin(9600);
   pinMode(8, OUTPUT);
   pinMode(9, OUTPUT);
+
 }
 
 void loop() {
   // put your main code here, to run repeatedly:
-  for(int i = 0; i < 20; i++){
+  //Setup start condition
+  digitalWrite(8, HIGH);
+  digitalWrite(9, HIGH);
+  delay(period);
+  // Start condition
+  digitalWrite(8, LOW);
+  digitalWrite(9, HIGH);
+  delay(period);
+  for(int i = 0; i < 5; i++){
     uint8_t index = 0x80;
     Serial.println(i);
     Serial.println();
     for(int o = 0; o < 8; o++){
-      digitalWrite(9, HIGH);
+      digitalWrite(9, LOW);
+      delay(period);
       digitalWrite(8, i & index);
+      digitalWrite(9, HIGH);
       Serial.println(i & index);
       index = index >> 1;
-      delay(20);
-      digitalWrite(9, LOW);
-      delay(20);
+      delay(period);
     }
     Serial.println();
     delay(1000);
   }
+  // Setup for stop condition
+  digitalWrite(8, LOW);
+  digitalWrite(9, HIGH);
+  delay(period);
+  // Stop condition
+  digitalWrite(8, HIGH);
+  digitalWrite(9, HIGH);
+  delay(period);
+  delay(5000);
 }
